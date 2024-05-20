ae5d7f32014e990f5b460d60f8eb978368abec54
Bastien VILLEGAS <95182085+Bastien64@users.noreply.github.com>
2024-05-20 18:10:36 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -34,84 +34,84 @@ def scrape_progressive():
     browser = webdriver.Chrome(options=chrome_options)
 
     results = []
+    result_item2_list = []
+    
 
     def progressive_extraction():
-        nonlocal results
-        action = ActionChains(browser)
-        prev_len = None  
-        same_len_count = 0 
-        processed_urls = {}  
+            nonlocal results
+            action = ActionChains(browser)
+            prev_len = None  
+            same_len_count = 0 
+            processed_urls = {}  
 
-        while True:
-            elements = browser.find_elements(By.CSS_SELECTOR, "a.hfpxzc")
-            if len(elements) == prev_len:
-                same_len_count += 1
-            else:
-                prev_len = len(elements)
-                same_len_count = 0
-            if same_len_count >= 3:
-                break
-
-            var = len(elements)
-            scroll_origin = ScrollOrigin.from_element(elements[-1])
-            action.scroll_from_origin(scroll_origin, 0, 30).perform()
-            time.sleep(2)
-
-        max_results = 10  # Limite de 10 résultats
-        results_count = 0
-
-        for i in range(min(10, len(elements))):  # Itérer sur un maximum de 10 éléments
-            try:
-                scroll_origin = ScrollOrigin.from_element(elements[i])
-                action.scroll_from_origin(scroll_origin, 0, 10).perform()
-                action.move_to_element(elements[i]).perform()
-                WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "hfpxzc")))
-                browser.execute_script("arguments[0].click();", elements[i])
-                time.sleep(5)
-                print(f"Successfully clicked on {i}")
-
-                # Récupérer le texte de l'élément h1
-                h1_element = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "DUwDvf")))
-                restaurant_name = h1_element.text
-
-                # Récupérer l'adresse
-                address_element = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div.Io6YTe.fontBodyMedium.kR99db")))
-                address = address_element.text
-
-                # Remplacer les abréviations dans l'adresse
-                address = address.replace("Pl.", "Place")
-                address = address.replace("Av.", "Avenue")
-                address = address.replace("Bd", "Boulevard")
-                address = address.replace("Ter", "T")
-                address = address.replace("Rte", "Route")
-                address = address.replace("Bis", "B")
-
-                # Récupérer le contenu de la page
-                page_source = browser.page_source
-
-                # Utiliser une expression régulière pour trouver le numéro de téléphone au format spécifique
-                phone_pattern = re.compile(r'(\d{2} \d{2} \d{2} \d{2} \d{2})')
-                matches = phone_pattern.findall(page_source)
-
-                if matches:
-                    phone_number = matches[0]  # Prendre le premier match trouvé
+            while True:
+                elements = browser.find_elements(By.CSS_SELECTOR, "a.hfpxzc")
+                if len(elements) == prev_len:
+                    same_len_count += 1
                 else:
-                    phone_number = None  # Aucun numéro de téléphone trouvé dans le format spécifié
+                    prev_len = len(elements)
+                    same_len_count = 0
+                if same_len_count >= 3:
+                    break
 
-                # Créer un dictionnaire avec les données récupérées
-                result_item = {
-                    "Nom de l'établissement": restaurant_name,
-                    "Addresse": address,
-                    "Téléphone": phone_number
-                }
-                results.append(result_item)
+                var = len(elements)
+                scroll_origin = ScrollOrigin.from_element(elements[-1])
+                action.scroll_from_origin(scroll_origin, 0, 30).perform()
+                time.sleep(2)
 
-            except Exception as e:
-                print(f"Error occurred while clicking element {i}: {e}")
+            max_results = 5
+            results_count = 0
 
-            results_count += 1
-            if results_count >= max_results:
-                break
+            for i in range(min(max_results, len(elements))): 
+                try:
+                    scroll_origin = ScrollOrigin.from_element(elements[i])
+                    action.scroll_from_origin(scroll_origin, 0, 10).perform()
+                    action.move_to_element(elements[i]).perform()
+                    WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "hfpxzc")))
+                    browser.execute_script("arguments[0].click();", elements[i])
+                    time.sleep(5)
+                    print(f"Successfully clicked on {i}")
+
+                    h1_element = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "DUwDvf")))
+                    restaurant_name = h1_element.text
+
+                    address_element = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div.Io6YTe.fontBodyMedium.kR99db")))
+                    address = address_element.text
+
+                    address = address.replace("Pl.", "Place")
+                    address = address.replace("Av.", "Avenue")
+                    address = address.replace("Bd", "Boulevard")
+                    address = address.replace("Ter", "T")
+                    address = address.replace("Rte", "Route")
+                    address = address.replace("Bis", "B")
+
+                    page_source = browser.page_source
+
+                    phone_pattern = re.compile(r'(\d{2} \d{2} \d{2} \d{2} \d{2})')
+                    matches = phone_pattern.findall(page_source)
+                    
+                    unique_matches = list(set(matches))
+                    print(unique_matches)
+
+                    if unique_matches:
+                        phone_number = unique_matches[0]  
+                    else:
+                        phone_number = None 
+
+                    result_item = {
+                        "Nom": restaurant_name,
+                        "Adresse": address,
+                        "Telephone": phone_number,
+                    }
+                    print(result_item)
+                    results.append(result_item)
+
+                except Exception as e:
+                    print(f"Error occurred while clicking element {i}: {e}")
+
+                results_count += 1
+                if results_count >= max_results:
+                    break
 
     browser.get(link)
     time.sleep(10)
@@ -121,64 +121,56 @@ def scrape_progressive():
     except Exception as e:
         print("Error occurred while clicking 'Accept All':", e)
 
+
     progressive_extraction()
-
-    # Une fois le scraping de Google Maps terminé, effectuez la recherche sur le site annuaire-entreprises.data.gouv.fr
+    final_results = []
     for result in results:
-        address = result["Addresse"]
 
-        # Aller sur le site annuaire-entreprises.data.gouv.fr
+        nom = result["Nom"]
+        address = result["Adresse"]
+        telephone = result["Telephone"]
+        print(f"Searching for {nom} at {address}")
         browser.get("https://annuaire-entreprises.data.gouv.fr/")
+        time.sleep(5)
+        search_input = browser.find_element(By.ID, "search-input-input")
+        search_input.clear() 
+        search_input.send_keys(address) 
+        search_input.send_keys(Keys.RETURN)  
 
-        # Attendre que la page se charge
         time.sleep(5)
 
-        # Trouver l'élément de recherche
-        search_input = browser.find_element(By.ID, "search-input-input")
 
-        # Insérer l'adresse récupérée dans le champ de recherche
-        search_input.clear()  # Assurez-vous que le champ est vide
-        search_input.send_keys(address)  # Insérer l'adresse récupérée
-        search_input.send_keys(Keys.RETURN)  # Appuyer sur la touche Entrée pour lancer la recherche
-
-        # Attendre que la page se charge après la recherche
-        try:
-            # Attendre que la page se charge après la recherche
-            wait = WebDriverWait(browser, 10)  # Attendre jusqu'à 10 secondes maximum
-            wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "style_result-item__fKcQt")))
-        except TimeoutException:
-            print("La recherche n'a pas abouti. Passer à la prochaine recherche.")
-            continue  # Passer à la prochaine itération de la boucle
+        address_cleaned = address.replace(",", "")
+        result_links = browser.find_elements(By.CSS_SELECTOR, "a.result-link")
+        print(f"Found {len(result_links)} result links")
 
 
-        # Maintenant, faire défiler la page vers le bas pour charger davantage de contenu
-        browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-        # Attendre un court instant pour que le contenu supplémentaire se charge
-        time.sleep(3)  # Vous pouvez ajuster ce délai en fonction de la vitesse de chargement de la page
-
-        # Extraire les données de la page
-        page_source = browser.page_source
-        soup = BeautifulSoup(page_source, "html.parser")
-        results_divs = soup.find_all("div", class_="style_result-item__fKcQt")
-        for div in results_divs:
-            badge = div.find("span", class_="styles_frBadge__s26DO")
-            if badge and "cessée" not in badge.text:
-                name_element = div.find("div", class_="style_dirigeants-or-elus__Qzy9P").find("span")
-                if name_element:
-                    name_parts = name_element.text.split("(")
-                    if len(name_parts) >= 2:
-                        nom = name_parts[0].strip()
-                        prenom = name_parts[1].replace(")", "").strip()
-                        print("Nom:", nom)
-                        print("Prénom:", prenom)
-
-        # Afficher un message dans la console pour indiquer que la recherche est terminée
-        print("Recherche papier réussie")
+        for link in result_links:
+            link_text = link.text
+            link_url = link.get_attribute("href")
+            print(f"Checking link: {link_text}")
+            link_address = link_text.split("\n")[-1].replace(",", "").lower()
+            if ("Hôtels et hébergement similaire" in link_text or "Débits de boissons" in link_text  or "Restauration traditionnelle" in link_text or "Restauration de type rapide" in link_text) and address_cleaned.lower() in link_address:
+                dirigeants_element = link.find_element(By.CSS_SELECTOR, "div.styles_icon__Qg4dr")
+                dirigeants_text = dirigeants_element.text
+                soup = BeautifulSoup(dirigeants_text, "html.parser")
+                nom_dirigeant = soup.get_text(strip=True)
+                result_item2 = {
+                    "Nom": nom,
+                    "Adresse": address,
+                    "Nom du dirigeant": nom_dirigeant,
+                    "Telephone": telephone,
+                }
+                result_item2_list.append(result_item2)
+                print(result_item2)
+                browser.execute_script(f"window.open('{link_url}', '_blank');")
+                print(f"Successfully opened the link for {nom}")
+                break
+        else:
+            print("No matching link found")
 
     browser.quit()
-
-    return json.dumps(results)  # retourner les résultats en tant que JSON
+    return render_template('index.html', results=result_item2_list)
 
 if __name__ == '__main__':
     app.run(debug=True)
